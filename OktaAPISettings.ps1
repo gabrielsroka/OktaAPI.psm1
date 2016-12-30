59912070b32792cbfbd0e0cfb048e7bbb995eced
gabrielsroka <gabrielsroka@gmail.com>
2016-12-29 17:04:49 -0800

    Update OktaAPISettings.ps1

--- OktaAPISettings.ps1
+++ OktaAPISettings.ps1
@@ -1,2 +1,5 @@
+# API token comes from Okta Admin > Security > API > Create Token
+# see http://developer.okta.com/docs/api/getting_started/getting_a_token.html
+
 # Call this before calling Okta API functions. Replace YOUR_API_TOKEN and YOUR_ORG with your values.
 Connect-Okta "YOUR_API_TOKEN" "https://YOUR_ORG.oktapreview.com"
