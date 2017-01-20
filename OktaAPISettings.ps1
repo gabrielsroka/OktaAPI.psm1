9826355519437f9b2aad87b93dd78714e6b7018f
gabrielsroka <gabrielsroka@gmail.com>
2017-01-19 16:05:23 -0800

    Update OktaAPISettings.ps1

--- OktaAPISettings.ps1
+++ OktaAPISettings.ps1
@@ -1,5 +1,8 @@
 # API token comes from Okta Admin > Security > API > Create Token
 # see http://developer.okta.com/docs/api/getting_started/getting_a_token.html
 
+# Tokens are valid for 30 days and automatically refresh with each API call. 
+# Tokens that are not used for 30 days will expire.
+
 # Call this before calling Okta API functions. Replace YOUR_API_TOKEN and YOUR_ORG with your values.
 Connect-Okta "YOUR_API_TOKEN" "https://YOUR_ORG.oktapreview.com"
